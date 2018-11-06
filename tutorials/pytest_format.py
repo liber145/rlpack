53421e9ed7456d84b706febc6a78fcd8dcb89e98
Unknown <7369396@qq.com>
2018-11-06 09:58:03 +0800

    add pytest format

---
+++ tutorials/pytest_format.py
@@ -0,0 +1,17 @@
+"""
+测试类的模板
+"""
+
+
+class MyCustomClass:
+    def __init__(self, attr):
+        self.attr = self._attr = attr
+
+    def sample(self, n):
+        return list(range(n))
+
+
+class MyCustomClass2(MyCustomClass):
+    def __init__(self, attr, new_attr):
+        super().__init__(attr)
+        self.new_attr = attr
